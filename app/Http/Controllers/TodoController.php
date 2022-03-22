15f8d15ec804c3f95dc5eed66f04b52500234d0e
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 02:46:54 +0430

    Added insert with unique user

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\Todo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
     public function index() {
-        $todos = Todo::latest()->get();
+        $todos = Todo::where('user', Auth::user()->id)->latest()->get();
 
         return view('index', ['todos' => $todos]);
+    }
+
+    public function add() {
+        $todo = new Todo();
+
+        $todo->name = request('todo');
+        $todo->user = Auth::user()->id;
+
+        $todo->save();
+
+        return redirect('/');
     }
 }

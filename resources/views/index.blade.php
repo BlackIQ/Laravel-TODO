3f2fa65dd517c1dff4c9906a7271c62a8cd7763f
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 02:47:43 +0430

    Added some new stuff

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,40 @@
 <div class="container">
     <h1>Welcome <b>{{ Auth::user()->name }}</b>!</h1>
     <br>
+    <form action="/add" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-md-11">
+                <input class="form-control" name="todo" placeholder="TODO name" id="todo" required>
+            </div>
+            <div class="col-md-1">
+                <button class="w-100 btn btn-primary">Add</button>
+            </div>
+        </div>
+        <br>
+    </form>
+    <br>
     @foreach ($todos as $todo)
-        <p>{{ $todo->name }}</p>
+        <div class="todo">
+            <h4>
+                {{ $todo->name }}
+                <div class="float-end">
+                    <button class="btn btn-primary">Edit</button>
+                    <button class="btn btn-link text-danger"><i class="fa fa-trash"></i></button>
+                </div>
+            </h4>
+            <div class="edit" style="display: none;">
+                <br>
+                <form action="/update/{{ $todo->id }}" method="POST">
+                    @csrf
+                    <label for="todo" class="form-label">Todo name</label>
+                    <input class="form-control" name="todo" placeholder="TODO name" id="todo" required>
+                    <br>
+                    <button class="btn btn-primary">Update</button>
+                </form>
+            </div>
+        </div>
+        <br>
     @endforeach
 </div>
 @endsection

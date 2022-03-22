a79eb872c62d02e058648c29b181c1a0932dca63
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 02:47:08 +0430

    Added user col

--- database/migrations/2022_03_22_212658_create_todos_table.php
+++ database/migrations/2022_03_22_212658_create_todos_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('details');
+            $table->integer('user');
             $table->timestamps();
         });
     }

accb1500edd377450cd1bc5462a0dda116816376
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 02:10:34 +0430

    Added get data as a sample

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <div class="container">
-    <h4>Welcome <b>{{ Auth::user()->name }}</b>!</h4>
+    <h1>Welcome <b>{{ Auth::user()->name }}</b>!</h1>
+    <br>
+    @foreach ($todos as $todo)
+        <p>{{ $todo->name }}</p>
+    @endforeach
 </div>
 @endsection

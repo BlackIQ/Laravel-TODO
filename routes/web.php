11d845d2ed03310844325d3bd4bbf62a1aced3f1
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 01:58:01 +0430

    Add route to index

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware('auth');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', function () {
+    return view('index');
+})->middleware('auth');

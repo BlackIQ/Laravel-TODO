9cddcf806b7ed06abe97089ab5be5bc325750cb7
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 01:50:11 +0430

    Added compile sass

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .react()
-    .sass('resources/sass/app.scss', 'public/css');
+// mix.js('resources/js/app.js', 'public/js')
+//     .react()
+//     .sass('resources/sass/app.scss', 'public/css');
+
+mix.sass('public/sass/app.sass', 'public/sass/app.css');

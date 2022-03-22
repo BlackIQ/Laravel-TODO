db22739c9cf7491e56faee7c1b4d2d5f0e19e534
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 02:47:25 +0430

    Added post to insert new data

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'App\Http\Controllers\TodoController@index')->middleware('auth');
+Route::post('/add', 'App\Http\Controllers\TodoController@add');
 
 Auth::routes();
 

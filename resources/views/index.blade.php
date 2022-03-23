de3872b43f6a41cb1d090e46fb60cb463d91aad5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-03-23 10:00:22 +0430

    Added checkbox

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -17,30 +17,39 @@
         <br>
     </form>
     <br>
-    @foreach ($todos as $todo)
-        <div class="todo">
-            <h4>
-                {{ $todo->name }}
-                <div class="float-end">
-                    <button class="btn btn-primary editing">Edit</button>
-                    <button class="btn btn-link text-danger" onclick="event.preventDefault(); document.getElementById('delete-form').submit();"><i class="fa fa-trash"></i></button>
-                </div>
-            </h4>
-            <div class="edit" style="display: none;">
-                <br>
-                <form action="/update/{{ $todo->id }}" method="POST">
-                    @csrf
-                    <label for="todo" class="form-label">Todo name</label>
-                    <input class="form-control" name="todo" placeholder="TODO name" id="todo" value="{{ $todo->name }}" required>
+    @if ($todos)
+        @foreach ($todos as $todo)
+            <div class="todo">
+                <h4>
+                    <input type="checkbox" class="form-check-input">
+                    @if ($todo->status == 'done')
+                        <s>{{ $todo->name }}</s>
+                    @else
+                        {{ $todo->name }}
+                    @endif
+                    <div class="float-end">
+                        <button class="btn btn-primary editing">Edit</button>
+                        <button class="btn btn-link text-danger" onclick="event.preventDefault(); document.getElementById('delete-form').submit();"><i class="fa fa-trash"></i></button>
+                    </div>
+                </h4>
+                <div class="edit" style="display: none;">
                     <br>
-                    <button class="btn btn-primary">Update</button>
+                    <form action="/update/{{ $todo->id }}" method="POST">
+                        @csrf
+                        <label for="todo" class="form-label">Todo name</label>
+                        <input class="form-control" name="todo" placeholder="TODO name" id="todo" value="{{ $todo->name }}" required>
+                        <br>
+                        <button class="btn btn-primary">Update</button>
+                    </form>
+                </div>
+                <form action="/delete/{{ $todo->id }}" id="delete-form" method="POST" class="d-none">
+                    @csrf
                 </form>
             </div>
-            <form action="/delete/{{ $todo->id }}" id="delete-form" method="POST" class="d-none">
-                @csrf
-            </form>
-        </div>
-        <br>
-    @endforeach
+            <br>
+        @endforeach
+    @else
+        <h4>No TODO is added yet.</h4>
+    @endif
 </div>
 @endsection

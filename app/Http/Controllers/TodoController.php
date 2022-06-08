6fb14d07166be06f066d4b6c8754bb40973f059f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-06-08 11:13:33 +0430

    Add comments in controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -8,83 +8,86 @@ use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller
 {
+    // Get all todos
     public function index() {
+        // Get todos from ORM
         $todos = Todo::where('user', Auth::user()->id)->latest()->get();
 
-        $all = Todo::where('user', Auth::user()->id)->count();
-        $dones = Todo::where('user', Auth::user()->id)->where('status', true)->count();
-        $notdones = Todo::where('user', Auth::user()->id)->where('status', false)->count();
-
-        return view('index', ['todos' => $todos, 'dones' => $dones, 'notdones' => $notdones, 'all' => $all]);
+        // Return index with todos
+        return view('index', ['todos' => $todos]);
     }
 
-    public function dones() {
-        $todos = Todo::where('user', Auth::user()->id)->where('status', true)->latest()->get();
-
-        $all = Todo::where('user', Auth::user()->id)->count();
-        $dones = Todo::where('user', Auth::user()->id)->where('status', true)->count();
-        $notdones = Todo::where('user', Auth::user()->id)->where('status', false)->count();
-
-        return view('done', ['todos' => $todos, 'dones' => $dones, 'notdones' => $notdones, 'all' => $all]);
-    }
-
-    public function notdones() {
-        $todos = Todo::where('user', Auth::user()->id)->where('status', false)->latest()->get();
-
-        $all = Todo::where('user', Auth::user()->id)->count();
-        $dones = Todo::where('user', Auth::user()->id)->where('status', true)->count();
-        $notdones = Todo::where('user', Auth::user()->id)->where('status', false)->count();
-
-        return view('not', ['todos' => $todos, 'dones' => $dones, 'notdones' => $notdones, 'all' => $all]);
-    }
-
+    // Add new task
     public function add() {
+        // Init a Todo
         $todo = new Todo();
 
+        // Add variables
         $todo->name = request('todo');
         $todo->user = Auth::user()->id;
         $todo->status = false;
 
+        // Insert in database
         $todo->save();
 
+        // Return index
         return redirect('/');
     }
 
+    // Update task
     public function update($id) {
+        // Find or return 404
         $todo = Todo::findOrFail($id);
 
+        // Change task name
         $todo->name = request('todo');
 
+        // Update it
         $todo->update();
 
+        // Return index
         return redirect('/');
     }
 
+    // Delete task
     public function delete($id) {
+        // Find or return 404
         $todo = Todo::findOrFail($id);
 
+        // Delete task
         $todo->delete();
 
+        // Return index
         return redirect('/');
     }
 
+    // Change status to not
     public function not($id) {
+        // Find or return 404
         $todo = Todo::findOrFail($id);
 
+        // Change to false | not
         $todo->status = false;
 
+        // Update
         $todo->update();
 
+        // Return index
         return redirect('/');
     }
 
+    // Change status to done
     public function done($id) {
+        // Find or return 404
         $todo = Todo::findOrFail($id);
 
+        // Change to true | done
         $todo->status = true;
 
+        // Update
         $todo->update();
 
+        // Return index
         return redirect('/');
     }
 }
